3976fe212e99822d9a7e8e4b358951e7ba052879
Cristina Defoe <cristina_defoe@yahoo.com>
2018-07-11 12:56:55 -0600

    Update files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,14 +13,39 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/about.html"));
   });
 
-  // Interior page
-  // app.get("/networking", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/networking.html"));
-  // });
-
   // Style page
   app.get("/style", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/style.html"));
+  });
+
+  // Whimsy page
+  app.get("/interior-whimsy", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/interior-whimsy.html"));
+  });
+
+  // Gather page
+  app.get("/interior-gathering", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/interior-gathering.html"));
+  });
+
+  //Sanctuary page
+  app.get("/interior-sanctuary", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/interior-sanctuary.html"));
+  });
+
+  //Kids page
+  app.get("/interior-kids-wing", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/interior-kids-wing.html"));
+  });
+
+  //D-Lounge page
+  app.get("/interior-d-lounge", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/interior-d-lounge.html"));
+  });
+
+  //Escape page
+  app.get("/interior-escape-nature", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/interior-escape-nature.html"));
   });
 
   // Blog page
